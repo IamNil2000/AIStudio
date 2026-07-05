783529d97b6d79edf7b6e48e2faa16ad4b682480
Goutam Jha <goutamkumarjha@gmail.com>
2026-07-05 21:28:56 +0530

    added the initial frontend setup to view GLTF object in web

--- src/components/EngineViewer.jsx
+++ src/components/EngineViewer.jsx
@@ -1,13 +1,38 @@
 
+// import { useGLTF } from "@react-three/drei";
+// import { useEffect } from "react";
+// import { Group } from "three/examples/jsm/libs/tween.module.js";
+
+// export default function EngineViewer() {
+//   const { scene } = useGLTF("models/EngineAsm/Engine.gltf");
+
+//   useEffect(() => {
+//     console.log(scene);
+//   }, [scene]);
+
+//   return <primitive object={scene} />;
+  
+// }
+
+
+
 import { useGLTF } from "@react-three/drei";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function EngineViewer() {
-  const { scene } = useGLTF("models/EngineAsm/Engine.gltf");
+  const { scene } = useGLTF("/models/EngineAsm/Engine.gltf");
+  const group = useRef();
 
   useEffect(() => {
     console.log(scene);
   }, [scene]);
 
-  return <primitive object={scene} />;
+  return (
+    <group
+      ref={group}
+      rotation={[-Math.PI/2, 0, 0]}
+    >
+      <primitive object={scene} />
+    </group>
+  );
 }
