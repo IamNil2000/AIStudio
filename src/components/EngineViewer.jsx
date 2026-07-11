9e382c72b0a65863cee0a685a2b292bf6d594666
Goutam Jha <goutamkumarjha@gmail.com>
2026-07-11 15:23:39 +0530

    added a simple design with exploded view

--- src/components/EngineViewer.jsx
+++ src/components/EngineViewer.jsx
@@ -1,37 +1,68 @@
 
 // import { useGLTF } from "@react-three/drei";
-// import { useEffect } from "react";
-// import { Group } from "three/examples/jsm/libs/tween.module.js";
+// import { useEffect, useRef } from "react";
 
 // export default function EngineViewer() {
-//   const { scene } = useGLTF("models/EngineAsm/Engine.gltf");
+//   const { scene } = useGLTF("/models/EngineAsm/Engine.gltf");
+//   const group = useRef();
 
 //   useEffect(() => {
 //     console.log(scene);
 //   }, [scene]);
 
-//   return <primitive object={scene} />;
-  
+//   return (
+//     <group
+//       ref={group}
+//       rotation={[-Math.PI/2, 0, 0]}
+//     >
+//       <primitive object={scene} />
+//     </group>
+//   );
 // }
-
-
 
 import { useGLTF } from "@react-three/drei";
 import { useEffect, useRef } from "react";
+import * as THREE from "three";
 
-export default function EngineViewer() {
-  const { scene } = useGLTF("/models/EngineAsm/Engine.gltf");
+export default function EngineViewer({ explodeAmount }) {
+  const { scene } = useGLTF("/models/Hooke/doublehookejoint.gltf");
   const group = useRef();
 
   useEffect(() => {
-    console.log(scene);
-  }, [scene]);
+    // Rotate model upright
+    scene.rotation.x = -Math.PI / 2;
+
+    // Compute model center
+    const box = new THREE.Box3().setFromObject(scene);
+    const center = box.getCenter(new THREE.Vector3());
+
+    scene.traverse((child) => {
+      if (!child.isMesh) return;
+
+      // Better looking material
+      child.material.metalness = 0.15;
+      child.material.roughness = 0.55;
+
+      // Save original position once
+      if (!child.userData.originalPosition) {
+        child.userData.originalPosition = child.position.clone();
+
+        child.userData.direction = child
+          .getWorldPosition(new THREE.Vector3())
+          .sub(center)
+          .normalize();
+      }
+
+      // Move according to explodeAmount
+      child.position.copy(child.userData.originalPosition);
+      child.position.add(
+        child.userData.direction.clone().multiplyScalar(explodeAmount)
+      );
+    });
+  }, [scene, explodeAmount]);
 
   return (
-    <group
-      ref={group}
-      rotation={[-Math.PI/2, 0, 0]}
-    >
+    <group ref={group}>
       <primitive object={scene} />
     </group>
   );
